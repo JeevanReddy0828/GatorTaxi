1b193e9bb1774e60424a2803dceee6aca47ff759
Bala Showri Jeevan Reddy Arlagadda <jeevanr.arlagadda@gmail.com>
2026-01-05 20:21:22 -0800

    Update stringUtils.h

--- stringUtils.h
+++ stringUtils.h
@@ -1,64 +1,73 @@
-#include<string>
-#include<iostream>
+#pragma once
 
-// prototypes functions
-void print(RBT *rbt, Heap *heap, int *args, string &output);
-void searchinRange(RBT *rbt, Heap *heap, int *args, string &output);
-void inputDrive(RBT *rbt, Heap *heap, int* args, string &output);
-void getNewDrive(RBT *rbt, Heap *heap, string &output);
-void terminateDrive(RBT *rbt, Heap *heap, int* args, string &output);
-void upgradeDrive(RBT *rbt, Heap *heap, int* args, string &output);
-void analyzeInput(RBT *rbt, Heap *heap, int argc, char **argv, string &output);
+#include <string>
+#include <iostream>
 
-// It just parse single arguments like print and cancel of drive
-void processArgs3(int *args, string line) {
-    int index = line.find_first_of("(") + 1, len = line.size() - index;
+using namespace std;
+
+class RBT;
+class Heap;
+
+// prototypes
+void Print(RBT* rbt, Heap* heap, int* args, string& output);
+void PrintRange(RBT* rbt, Heap* heap, int* args, string& output);
+void InsertRide(RBT* rbt, Heap* heap, int* args, string& output);
+void GetNextRide(RBT* rbt, Heap* heap, string& output);
+void CancelRide(RBT* rbt, Heap* heap, int* args, string& output);
+void UpdateTrip(RBT* rbt, Heap* heap, int* args, string& output);
+
+static inline void processArgs3(int* args, const string& line) {
+    int index = (int)line.find_first_of("(") + 1;
+    int len = (int)line.size() - index;
     string argStr = line.substr(index, len - 1);
 
-    int firstComma = argStr.find_first_of(",");
+    int firstComma = (int)argStr.find_first_of(",");
     args[0] = stoi(argStr.substr(0, firstComma));
 
-    int secondComma = argStr.find_last_of(",");
-    if(secondComma == string::npos) return;
-    args[1] = stoi(argStr.substr(firstComma + 1, secondComma - firstComma));
+    int secondComma = (int)argStr.find_last_of(",");
+    if (secondComma == (int)string::npos) return;
+
+    args[1] = stoi(argStr.substr(firstComma + 1, secondComma - firstComma - 1));
     args[2] = stoi(argStr.substr(secondComma + 1));
 }
-//it just analyzes the arguments to upgrade command 
-void processArgs2(int *args, string line) {
-    int index = line.find_first_of("(") + 1, len = line.size() - index;
+
+static inline void processArgs2(int* args, const string& line) {
+    int index = (int)line.find_first_of("(") + 1;
+    int len = (int)line.size() - index;
     string argStr = line.substr(index, len - 1);
 
-    int firstComma = argStr.find_first_of(",");
+    int firstComma = (int)argStr.find_first_of(",");
     args[0] = stoi(argStr.substr(0, firstComma));
-    args[1] = stoi(argStr.substr(firstComma + 1));    
-}
-//it just analyzes the arguments to insert values
-void processArgs1(int *args, string line) {
-    int index = line.find_first_of("(") + 1, len = line.size() - index;
-    args[0] = stoi(line.substr(index, len - 1)); 
+    args[1] = stoi(argStr.substr(firstComma + 1));
 }
 
-// parses input to call necessary operation routines and parse arguments.
-void processOperation(RBT *rbt, Heap *heap, string line, string operationPrefix, string &output) {
-    int args[3];
-    if(operationPrefix == "Pri") {
-        if(line.find(",") != string::npos) {
+static inline void processArgs1(int* args, const string& line) {
+    int index = (int)line.find_first_of("(") + 1;
+    int len = (int)line.size() - index;
+    args[0] = stoi(line.substr(index, len - 1));
+}
+
+static inline void processOperation(RBT* rbt, Heap* heap, const string& line, const string& operationPrefix, string& output) {
+    int args[3] = {0, 0, 0};
+
+    if (operationPrefix == "Pri") {
+        if (line.find(",") != string::npos) {
             processArgs2(args, line);
-            printRange(rbt, heap, args, output);
+            PrintRange(rbt, heap, args, output);
         } else {
             processArgs1(args, line);
-            print(rbt, heap, args, output);
+            Print(rbt, heap, args, output);
         }
-    } else if(operationPrefix == "Ins") {
+    } else if (operationPrefix == "Ins") {
         processArgs3(args, line);
-        insertRide(rbt, heap, args, output);
-    } else if(operationPrefix == "Upd") {
+        InsertRide(rbt, heap, args, output);
+    } else if (operationPrefix == "Upd") {
         processArgs2(args, line);
-        updateTrip(rbt, heap, args, output);
-    } else if(operationPrefix == "Can") {
+        UpdateTrip(rbt, heap, args, output);
+    } else if (operationPrefix == "Can") {
         processArgs1(args, line);
-        cancelRide(rbt, heap, args, output);
-    } else if(operationPrefix == "Get") {
-        getNextRide(rbt, heap, output);
+        CancelRide(rbt, heap, args, output);
+    } else if (operationPrefix == "Get") {
+        GetNextRide(rbt, heap, output);
     }
 }
