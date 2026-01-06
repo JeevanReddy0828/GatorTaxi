f4d03ee2b96f709c90952808573fe588c6d5d360
Bala Showri Jeevan Reddy Arlagadda <jeevanr.arlagadda@gmail.com>
2026-01-05 20:19:42 -0800

    Updateing MyHeap.h

--- MyHeap.h
+++ MyHeap.h
@@ -1,16 +1,18 @@
-#include<iostream>
-#include<math.h>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
 
 using namespace std;
 
 class RBTNode;
 
 class HeapNode {
-public :
-    RBTNode *rbtNode; //a pointer to an object of the RBTNode class named rbtNode
-    int index, DriveNumber, DriveCost, DriveDuration; //variables
-    HeapNode() { // constructor function
+public:
+    RBTNode* rbtNode;
+    int index, DriveNumber, DriveCost, DriveDuration;
+
+    HeapNode() {
         index = -1;
         DriveNumber = 0;
         DriveCost = 0;
@@ -18,146 +20,165 @@ public :
         rbtNode = nullptr;
     }
 
-    HeapNode(RBTNode *rbtNode, int DriveNumber, int DriveCost, int DriveDuration) { //constructor function
-        this -> index = -1;
-        this -> rbtNode = rbtNode; 
-        this -> DriveNumber = DriveNumber;
-        this -> DriveCost = DriveCost;
-        this -> DriveDuration = DriveDuration;
+    HeapNode(RBTNode* rbtNode, int DriveNumber, int DriveCost, int DriveDuration) {
+        this->index = -1;
+        this->rbtNode = rbtNode;
+        this->DriveNumber = DriveNumber;
+        this->DriveCost = DriveCost;
+        this->DriveDuration = DriveDuration;
     }
 
-    void printHeapNode() { //print function that prints the information of Heap Node
-        cout << "(" << this -> index << ", " << 
-        this -> DriveNumber << ", " << 
-        this -> DriveCost << ", " << 
-        this -> DriveDuration << ")";
+    void printHeapNode() {
+        cout << "(" << this->index << ", "
+             << this->DriveNumber << ", "
+             << this->DriveCost << ", "
+             << this->DriveDuration << ")";
     }
 
-    void printHeapNode(string &output) {
-        output += "(" + to_string(this -> DriveNumber) + "," + 
-        to_string(this -> DriveCost) + "," +  
-        to_string(this -> DriveDuration) + ")\n";
+    void printHeapNode(string& output) {
+        output += "(" + to_string(this->DriveNumber) + "," +
+                  to_string(this->DriveCost) + "," +
+                  to_string(this->DriveDuration) + ")\n";
     }
 };
 
 class Heap {
-    int back;
-    HeapNode* heap[2000];
+    vector<HeapNode*> heap;
+
+    static bool lessNode(const HeapNode* a, const HeapNode* b) {
+        if (a->DriveCost != b->DriveCost) return a->DriveCost < b->DriveCost;
+        return a->DriveDuration < b->DriveDuration;
+    }
+
+    void swapAt(int i, int j) {
+        std::swap(heap[i], heap[j]);
+        heap[i]->index = i;
+        heap[j]->index = j;
+    }
+
+    void heapifyUp(int i) {
+        while (i > 0) {
+            int p = (i - 1) / 2;
+            if (!lessNode(heap[i], heap[p])) break;
+            swapAt(i, p);
+            i = p;
+        }
+    }
+
+    void heapifyDown(int i) {
+        int n = (int)heap.size();
+        while (true) {
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+            int best = i;
+
+            if (l < n && lessNode(heap[l], heap[best])) best = l;
+            if (r < n && lessNode(heap[r], heap[best])) best = r;
+
+            if (best == i) break;
+            swapAt(i, best);
+            i = best;
+        }
+    }
+
 public:
-    Heap() {
-        back = 0;
+    Heap() = default;
+
+    int getSize() const {
+        return (int)heap.size();
     }
 
-    int getSize() { // function that returns the value of the back variable
-        return back;
+    void push(HeapNode* node) {
+        node->index = (int)heap.size();
+        heap.push_back(node);
+        heapifyUp(node->index);
     }
 
-    void push(HeapNode *heapNode) {
-        heap[back] = heapNode;
-        heap[back] -> index = back;
-        heapifyUp(back);
-        back++;
-    }
-
-    void getTopNode () { // function that returns the value of the topHeap node
-        heap[0] -> printHeapNode();
-    }
-    
-    HeapNode* top () { //function that returns the HeapNode object
+    HeapNode* top() {
+        if (heap.empty()) return nullptr;
         return heap[0];
     }
 
-    HeapNode* pop() { // removes and then returns the HeapNode object at the top of the heap.
-        back--;
-        HeapNode *deletedNode = heap[0];
+    void getTopNode() {
+        if (!heap.empty()) heap[0]->printHeapNode();
+    }
 
-        heap[0] = heap[back];
-        heap[0] -> index = 0;
+    HeapNode* pop() {
+        if (heap.empty()) return nullptr;
+
+        HeapNode* deleted = heap[0];
+        int last = (int)heap.size() - 1;
+
+        if (last == 0) {
+            heap.pop_back();
+            deleted->index = -1;
+            return deleted;
+        }
+
+        heap[0] = heap[last];
+        heap[0]->index = 0;
+        heap.pop_back();
+
+        deleted->index = -1;
         heapifyDown(0);
-        return deletedNode;
+        return deleted;
     }
 
-    void heapifyUp(int childIndex) { //recursive function
-        if(childIndex == 0) return ;
-        int parent = childIndex % 2 ? (childIndex - 1)/2 : (childIndex - 1)/2;
+    void update(HeapNode* node, int newCost, int newDuration) {
+        if (!node) return;
+        int idx = node->index;
+        if (idx < 0 || idx >= (int)heap.size()) return;
 
-        int prc = heap[parent] -> DriveCost, crc = heap[childIndex] -> DriveCost;
-        int ptd = heap[parent] -> DriveDuration, ctd = heap[childIndex] -> DriveDuration;
-        
-        if(prc < crc) return;
+        int oldCost = node->DriveCost;
+        int oldDur  = node->DriveDuration;
 
-        if(prc > crc) {
-            swap(heap[parent] -> index, heap[childIndex] -> index); //swaps the index variables 
-            swap(heap[parent], heap[childIndex]);
-        } else if(ptd > ctd) {
-            swap(heap[parent] -> index, heap[childIndex] -> index);
-            swap(heap[parent], heap[childIndex]);
-        } else return ;
+        node->DriveCost = newCost;
+        node->DriveDuration = newDuration;
 
-        heapifyUp(parent); //recursively call heapifyUp
-    }
-
-    void heapifyDown(int parent) { //recursive function
-        int leftChild = parent * 2 + 1, rightChild = parent * 2 + 2;
-        if(leftChild >= back && rightChild >= back) return;
-
-        int prc = heap[parent] -> DriveCost, 
-            lcrc = heap[leftChild] -> DriveCost;
-        int ptd = heap[parent] -> DriveDuration, 
-            lctd = heap[leftChild] -> DriveDuration;
-        int childToSwap = rightChild;
-
-        //  lc = exists or not
-        if(rightChild == back) {
-            if(prc > lcrc || ptd > lctd) childToSwap = leftChild;
+        // decide direction safely
+        int p = (idx - 1) / 2;
+        if (idx > 0 && lessNode(heap[idx], heap[p])) {
+            heapifyUp(idx);
         } else {
-            int rcrc = heap[rightChild] -> DriveCost, 
-                rctd = heap[rightChild] -> DriveDuration;
-
-            if(prc < lcrc && prc < rcrc) return;
-
-            if(prc > lcrc || ptd > lctd) childToSwap = leftChild;
-            else if(prc > rcrc || ptd > rctd) childToSwap = rightChild;
+            heapifyDown(idx);
         }
-        
-        swap(heap[parent] -> index, heap[childToSwap] -> index);
-        swap(heap[parent], heap[childToSwap]);
-        heapifyDown(childToSwap);
+
+        (void)oldCost;
+        (void)oldDur;
     }
 
-    void update(HeapNode* heapNode, int newDriveCost, int newDriveDuration) { //updates a node in the heap array 
-        if(newDriveCost > heapNode -> DriveCost) {
-            heapNode -> DriveCost = newDriveCost;
-            heapifyDown(heapNode -> index);
-        } else if(newDriveDuration <= heapNode -> DriveDuration) {
-            heapNode -> DriveDuration = newDriveDuration;
-            heapifyUp(heapNode -> index);
-        } else if(newDriveDuration > heapNode -> DriveDuration) {
-            heapNode -> DriveDuration = newDriveDuration;
-            heapifyDown(heapNode -> index);
+    void remove(HeapNode* node) {
+        if (!node) return;
+        int idx = node->index;
+        if (idx < 0 || idx >= (int)heap.size()) return;
+
+        int last = (int)heap.size() - 1;
+        node->index = -1;
+
+        if (idx == last) {
+            heap.pop_back();
+            return;
+        }
+
+        heap[idx] = heap[last];
+        heap[idx]->index = idx;
+        heap.pop_back();
+
+        // could go up or down
+        if (idx > 0) {
+            int p = (idx - 1) / 2;
+            if (lessNode(heap[idx], heap[p])) heapifyUp(idx);
+            else heapifyDown(idx);
+        } else {
+            heapifyDown(idx);
         }
     }
 
-    void remove(HeapNode* heapNode) { //removes node from heap array
-        int index = heapNode -> index;
-
-        back--;
-        heap[index] = heap[back];
-        heap[index] -> index = index;
-        heapifyDown(index);
-    }
-
-    void printHeap() { //prints the heap array.
-        int level = 0, limit = 0;
-
-        for(int i = 0; i < back; i++) {
-            heap[i] -> printHeapNode();
-            cout << ", ";
-            if(i == limit) {
-                limit += pow(2, ++level);
-                cout << endl;
-            }
+    void printHeap() {
+        for (int i = 0; i < (int)heap.size(); i++) {
+            heap[i]->printHeapNode();
+            cout << (i + 1 < (int)heap.size() ? ", " : "");
         }
-        cout << endl; 
+        cout << "\n";
     }
 };
